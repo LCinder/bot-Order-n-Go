708197e2f559e4f9ff21acd84af401149c340fbf
Diego Hernandez Moreno <diegohm1598@gmail.com>
2020-11-24 18:37:44 +0100

    actualizado index

--- functions/index.js
+++ functions/index.js
@@ -3,18 +3,18 @@ const Telegraf = require("telegraf");
 const bot = new Telegraf(process.env.BOT_KEY);
 
 bot.command("mesa", (ctx) => {
-		ctx.reply("Mesa:\n\n");
+		ctx.reply("Mesa\n\n");
 });
 
 bot.command("pedido", (ctx) => {
-		ctx.reply("Pedido:\n\n");
+		ctx.reply("Pedido\n\n");
 });
 
 
 bot.help((ctx) => {
-	ctx.reply("Comandos:\n\n");
+	ctx.reply("Comandos\n\n");
 	ctx.reply("	1.- /mesa - Indica la mesa con todos los pedidos de la misma.\n");
-	ctx.reply("	2.- /pedidos <nº pedido> - Indica la información del nº de pedido especificado.\n");
+	ctx.reply("	2.- /pedidos no pedido - Indica la informacion del n de pedido especificado.\n");
 });
 
 bot.launch();
