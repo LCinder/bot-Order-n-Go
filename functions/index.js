12e11e526c5aa719bb6aacd6cea1dfb8e50c4afd
Diego Hernandez Moreno <diegohm1598@gmail.com>
2020-11-27 17:29:13 +0100

    cambiado funciton en bot

--- functions/index.js
+++ functions/index.js
@@ -18,7 +18,7 @@ bot.help((ctx) => {
 });
 
 
-exports.handler = async function(event, context, callback) => {
-	bot.handleUpdate(JSON.parse(event.body));
+exports.handler = async (event, context, callback) => {
+	await bot.handleUpdate(JSON.parse(event.body));
 	return callback(null, {statusCode: 200, body: ""});
 };
